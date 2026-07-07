b802789cab2737da0ea3314bd0d5aeefb5925cf4
mduvalhall <mduvalhall@proton.me>
2026-07-07 17:40:59 +0100

    working on rewriting correctly

---
+++ grammar_old.js
@@ -0,0 +1,56 @@
+module.exports = grammar({
+    name: "tbd",
+
+    extras: ($) => [/\s/, $.comment],
+
+    word: ($) => $.symbol,
+
+    rules: {
+        source_file: ($) => repeat($._expr),
+
+        comment: (_) => token(seq("--", /.*/)),
+
+        _expr: ($) =>
+            choice(
+                $.call,
+                $.list,
+                $.tuple,
+                $.map,
+                $.binary,
+                $.integer,
+                $.string,
+                $.bool,
+                $.constructor,
+                $.module,
+                $.symbol,
+            ),
+
+        call: ($) =>
+            seq(
+                "(",
+                field("callee", $._expr),
+                repeat(field("argument", $._expr)),
+                ")",
+            ),
+
+        list: ($) => seq("[", repeat($._expr), "]"),
+
+        tuple: ($) => seq("{", repeat($._expr), "}"),
+
+        _field: ($) => seq(field("key", $._expr), field("value", $.expr)),
+        map: ($) => seq("#{", repeat($._field), "}"),
+
+        integer: (_) => token(/-?(0|[1-9][0-9]*)/),
+
+        string: (_) => token(seq('"', repeat(choice(/[^"\\]/, /\\./)), '"')),
+
+        bool: (_) => choice("true", "false"),
+
+        constructor: (_) => token(/\$[A-Za-z0-9_-]*/),
+
+        module: ($) => seq(token(/[A-Za-z0-9_]*\:/), $.call),
+
+        symbol: (_) =>
+            token(/[a-z_+\-*/=<>!?$%&~^:][A-Za-z0-9_+\-*/=<>!?$%&~^:.]*/),
+    },
+});
